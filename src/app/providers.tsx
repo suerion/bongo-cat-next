dff7daec4e4d7ea1042f27f08b0e14247a2a77ce
suerion <55131959+suerion@users.noreply.github.com>
2026-02-23 23:57:24 +0100

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,33 +1,17 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React from "react";
 import { ConfigProvider } from "antd";
 import { I18nextProvider } from "react-i18next";
 import i18n from "@/i18n";
+import { I18nDebug } from "@/components/I18nDebug";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    const lng = i18n.resolvedLanguage ?? i18n.language;
-
-    console.log("[i18n] language:", i18n.language);
-    console.log("[i18n] resolvedLanguage:", i18n.resolvedLanguage);
-    console.log("[i18n] languages:", i18n.languages);
-
-    console.log("[i18n] has menu:", i18n.hasResourceBundle(lng, "menu"));
-    console.log("[i18n] has system:", i18n.hasResourceBundle(lng, "system"));
-
-    console.log("[i18n] t(scale.title @menu):", i18n.t("scale.title", { ns: "menu" }));
-    console.log("[i18n] t(system:hideCat):", i18n.t("system:hideCat"));
-
-    // Extra: harter Test auf en-US (um "en" vs "en-US" sofort zu entlarven)
-    console.log("[i18n] has en-US/menu:", i18n.hasResourceBundle("en-US", "menu"));
-    console.log("[i18n] t(scale.title @en-US):", i18n.t("scale.title", { ns: "menu", lng: "en-US" }));
-  }, []);
-
   return (
     <I18nextProvider i18n={i18n}>
       <ConfigProvider theme={{ token: { colorPrimary: "#1890ff" } }}>
         {children}
+        <I18nDebug />
       </ConfigProvider>
     </I18nextProvider>
   );
