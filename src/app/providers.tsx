f863adae160f2aebf15efc0f79b207802d8a27b4
suerion <55131959+suerion@users.noreply.github.com>
2026-03-24 23:00:52 +0100

    Add Toaster component to providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 import { I18nextProvider } from "react-i18next";
 import i18n, { i18nReady } from "@/i18n";
+import { Toaster } from "@/components/ui/sonner";
 
 const I18nDebug = dynamic(
   () => import("@/components/I18nDebug").then((mod) => mod.I18nDebug),
@@ -35,6 +36,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <I18nextProvider i18n={i18n}>
       {children}
+      <Toaster />
       <I18nDebug />
     </I18nextProvider>
   );
