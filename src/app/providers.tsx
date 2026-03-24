a3237f62c02c946585b3a2545fec0527f262bf9f
suerion <55131959+suerion@users.noreply.github.com>
2026-03-24 22:20:50 +0100

    Remove ConfigProvider from providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { ConfigProvider } from "antd";
 import { I18nextProvider } from "react-i18next";
 import i18n, { i18nReady } from "@/i18n";
 import { I18nDebug } from "@/components/I18nDebug";
@@ -30,10 +29,8 @@ export default function Providers({ children }: { children: React.ReactNode }) {
 
   return (
     <I18nextProvider i18n={i18n}>
-      <ConfigProvider theme={{ token: { colorPrimary: "#1890ff" } }}>
-        {children}
-        <I18nDebug />
-      </ConfigProvider>
+      {children}
+      <I18nDebug />
     </I18nextProvider>
   );
 }
