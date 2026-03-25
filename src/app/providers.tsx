91b9d43102a6b273bac3121242dcc34d1674c5f4
suerion <55131959+suerion@users.noreply.github.com>
2026-03-25 01:47:38 +0100

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,8 +2,6 @@
 
 import React from "react";
 import dynamic from "next/dynamic";
-import { I18nextProvider } from "react-i18next";
-import i18n from "@/i18n";
 import { Toaster } from "@/components/ui/sonner";
 
 const I18nDebug = dynamic(
@@ -13,10 +11,10 @@ const I18nDebug = dynamic(
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <I18nextProvider i18n={i18n}>
+    <>
       {children}
       <Toaster />
       <I18nDebug />
-    </I18nextProvider>
+    </>
   );
 }
